1aef93724f6efb12e0640c086e5a3cb67a2e19f6
Lai Khanh Hoang <laikhanhhoang3011@gmail.com>
2026-09-17 09:08:30 +0700

    commit to save my work

---
+++ JavaScript - 30 Days/2666. Allow One Function Call.js
@@ -0,0 +1,17 @@
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+    return function(...args) {
+        if (called) return undefined;
+        called = true;
+        return fn(...args);
+    };
+};
---
+++ JavaScript - 30 Days/2703. Return Length of Arguments Passed.js
@@ -0,0 +1,12 @@
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+
+console.log(argumentsLength(1, 2, 3)); // 3
